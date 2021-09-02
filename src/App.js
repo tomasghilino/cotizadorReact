df4dad1b33a9a5cb13e56f93cdfddcacf290e54c
Tomas Ghilino <tomasghilino01@gmail.com>
2021-09-01 21:00:20 -0300

    subo todo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Header from './components/Headers';
+import Formulario from './components/Formulario';
+import Resumen from './components/Resumen';
+import styled from '@emotion/styled';
+
+import Resultado from './components/Resultado';
+import Spinner from './components/Spinner';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #ffffff;
+  padding: 3rem;
+`;
 
 function App() {
+  const [resumen, guardarResumen] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: '',
+      year: '',
+      plan: '',
+    },
+  });
+
+  const [cargando, guardarCargando] = useState(false);
+
+  const { cotizacion, datos } = resumen;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Header titulo="Cotizador de Seguros" />
+
+      <ContenedorFormulario>
+        <Formulario
+          guardarResumen={guardarResumen}
+          guardarCargando={guardarCargando}
+        />
+        {cargando ? <Spinner /> : null}
+        {!cargando ? <Resumen datos={datos} /> : null}
+
+        {!cargando ? <Resultado cotizacion={cotizacion} /> : null}
+      </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
---
+++ src/helper.js
@@ -0,0 +1,34 @@
+// obtiene diferencia de años
+export function obtenerDiferenciaYear(year) {
+  return new Date().getFullYear() - year;
+}
+
+//calcula el total a pagar segun la marca
+export function calcularMarca(marca) {
+  let incremento;
+  switch (marca) {
+    case 'europeo':
+      incremento = 1.3;
+      break;
+    case 'americano':
+      incremento = 1.15;
+      break;
+    case 'asiatico':
+      incremento = 1.05;
+      break;
+
+    default:
+      break;
+  }
+  return incremento;
+}
+
+// calcula el tipo de seguro
+export function obtenerPlan(plan) {
+  return plan === 'basico' ? 1.2 : 1.5;
+}
+
+// muestra la primer letra mayuscula
+export function primerMayuscula(texto) {
+  return texto.charAt(0).toUpperCase() + texto.slice(1);
+}
